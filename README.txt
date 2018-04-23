5523e5f21d7c98703435e22037efff269ec6dd91
silverware13 <thomasza@oregonstate.edu>
2018-04-22 19:34:35 -0700

    improved handle

---
+++ README.txt
@@ -0,0 +1,2 @@
+Begin by compiling chatclient.c with the following command:
+gcc -o chatclient chatclient.c
--- chatclient.c
+++ chatclient.c
@@ -19,28 +19,23 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <stdbool.h>
 
 //function prototype(s)
-void get_handle();
+bool check_args(int argc, char *argv[]);
+void get_handle(char *handle);
 void setup_connection();
 void chat();
 
 int main(int argc, char *argv[])
 {
-	//make sure the user entered correct number of arguments.
-	if(argc < 3){
-		printf("useage: %s [host name] [port number]\n", argv[0]);
+	//make sure the user passed valid arguments
+	if(!check_args(argc, argv)){
 		return 0;
 	}
 
-	//make sure the user has entered the port number as digits
-	if(!isdigit(*argv[2])){
-		printf("Please enter port number as an unsigned integer.\n");
-		return 0;
-	}
-	
+	//set variables	
 	int port_num = strtol(argv[2], NULL, 10);
-
 	char *handle = handle;
 	handle = (char *)malloc(MAX_CHARS_HANDLE * sizeof(char));
 
@@ -48,7 +43,7 @@ int main(int argc, char *argv[])
 	get_handle(handle);
 	printf("This is your handle: %s\n", handle);	
 
-	//setup the connection to server
+	//setup the connection with the server
 	setup_connection();
 
 	//start chatting with the host
@@ -57,18 +52,32 @@ int main(int argc, char *argv[])
 	return 0;
 }
 
+bool check_args(int argc, char *argv[])
+{
+	//make sure the user entered correct number of arguments.
+	if(argc < 3){
+		printf("useage: %s [host name] [port number]\n", argv[0]);
+		return false;
+	}
+
+	//make sure the user has entered the port number as digits
+	if(!isdigit(*argv[2])){
+		printf("Please enter port number as an unsigned integer.\n");
+		return false;
+	}
+	
+	return true;
+}
+
 void get_handle(char *handle)
 {
-	while(true){
-		printf("Please enter a handle, it must be no longer than %d characters.\nHandle: ", MAX_CHARS_HANDLE);
-		scanf("%s", handle, 10);
-		return;	
-	}
+	printf("Please enter a handle, it must be no longer than %d characters.\nHandle: ", MAX_CHARS_HANDLE);
+	scanf("%10s", handle, 10);
 }
 
 void setup_connection()
 {
-
+		
 }
 
 void chat()
