5d866fb9366cee18e0920287138545b695a9a791
silverware13 <thomasza@oregonstate.edu>
2018-04-29 00:49:54 -0700

    added handle for py

--- chatserve.py
+++ chatserve.py
@@ -14,23 +14,37 @@
 from socket import *
 import sys
 
+#confirm command line arguments include port number
 if (len(sys.argv) < 2 or len(sys.argv) > 2):
 	print "useage: " + str(sys.argv[0]) + " [port number]"
 	sys.exit(0)
+
+#get handle
+print "Please enter a handle, it must be no longer than 10 characters."
+handle = raw_input("Handle: ")
+handle = handle[:10] #make sure handle is not longer than 10 characters
+print "Your handle is: " + handle
+
+#setup socket and begin listening 
 serverPort = int(sys.argv[1])
 serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind(('', serverPort))
 serverSocket.listen(1)
+
+#wait to accept incoming connection
 while 1:
 	print "The server is waiting to receive connection."
 	connectionSocket, addr = serverSocket.accept()
 
+	#we made a connection keep sending messages to and from client
 	while 1:
 		clientMessage = connectionSocket.recv(1024)
 		clientMessage = clientMessage.rstrip()
 		print clientMessage
-		serverMessage = raw_input("server> ")
-		connectionSocket.send("server> " + serverMessage + '\n')
+		serverMessage = raw_input(handle + "> ")
+		connectionSocket.send(handle + "> " + serverMessage + '\n')
+
+		#if /quit has been used go back to listening for connections
 		if(serverMessage == "/quit"):
 			connectionSocket.close()
 			break
